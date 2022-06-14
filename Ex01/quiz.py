5dd03cc1bccbb12a1eba008a8e1fcc86439feda6
RinShigekane <c0a2106423@edu.teu.ac.jp>
2022-06-14 16:25:57 +0900

    fix Ex01/quiz.py #2

--- Ex01/quiz.py
+++ Ex01/quiz.py
@@ -2,7 +2,9 @@ if __name__ == "__main__":
     import random
 
     def shutudai():
-        #Q_COUNT = 3
+        Q_COUNT = 3
+        q_list =list()
+        
         ans = list() 
         ans.append(("マスオ", "ますお"))
         ans.append(("ワカメ", "わかめ"))
@@ -16,19 +18,22 @@ if __name__ == "__main__":
         for i in range(Q_COUNT):
             q_list.append((quiz[i], ans[i]))
         
-        #q_num = random.randint(0,Q_COUNT-1)
+        q_num = random.randint(0,Q_COUNT-1)
         print(q_list[q_num][0])
+
+        return q_list[q_num][1]
     
-    def kaito():
-        Usr_ans = input("答えを入力 : ")
-        if Usr_ans in q_list[q_num][1]:
+    def kaito(ans):
+        usr_ans = input("答えを入力 : ")
+        #if Usr_ans in q_list[q_num][1]:
+        if usr_ans in ans:
             print("正解です！")
         else:
             print("違います。")
 
-    Q_COUNT = 3
-    q_list = list()
-    q_num = random.randint(0,Q_COUNT-1)
+    #Q_COUNT = 3
+    #q_list = list()
+    #q_num = random.randint(0,Q_COUNT-1)
     #q_num = 0
-    shutudai()
-    kaito()
+    a = shutudai()
+    kaito(a)
