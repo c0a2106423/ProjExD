ae92cd57e8906ec90411e2cacc078bb942cb7306
RinShigekane <c0a2106423@edu.teu.ac.jp>
2022-06-28 14:22:08 +0900

    問２

--- Ex03/maze.py
+++ Ex03/maze.py
@@ -2,10 +2,13 @@ import tkinter as tk
 import tkinter.messagebox as tkm
 
 def main():
+    global root, koukaton, cx, cy
     root = tk.Tk()
     root.title("迷えるこうかとん")
     root.geometry("1500x900")
-
+    koukaton = tk.PhotoImage(file="5.png")
+    cx, cy = 300, 400
+    tk.Canvas.create_image(cx, cy, image=koukaton, tag="koukaton")
     root.mainloop()    
 
 if __name__ == "__main__":
