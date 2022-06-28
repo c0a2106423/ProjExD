b269592d8ac53163f1d95af3b68676adb4ac74e9
RinShigekane <c0a2106423@edu.teu.ac.jp>
2022-06-28 14:32:50 +0900

    問6

--- Ex03/maze.py
+++ Ex03/maze.py
@@ -1,8 +1,10 @@
+import re
 import tkinter as tk
 import tkinter.messagebox as tkm
 
 def main():
-    global root, koukaton, cx, cy
+    global root, koukaton, cx, cy, key
+    key = ""
     root = tk.Tk()
     root.title("迷えるこうかとん")
     canvas = tk.Canvas(
@@ -17,7 +19,23 @@ def main():
     cx, cy = 300, 400
     canvas.create_image(cx, cy, image=koukaton, tag="koukaton")
 
+    root.bind("<KeyPress>", key_down)
+    root.bind("<KeyRelease>", key_up)
     root.mainloop()    
+
+def key_down(event):
+    global key
+    key = event.keysym
+    print(key)
+    return
+
+def key_up(event):
+    global key
+    key = ""
+    print(key)
+    return
+
+
 
 if __name__ == "__main__":
     main()
