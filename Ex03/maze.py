ece8ca628160e273f6ca5c361a9bb52ae0cc4dcd
RinShigekane <c0a2106423@edu.teu.ac.jp>
2022-06-28 14:14:31 +0900

    問２

---
+++ Ex03/maze.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+import tkinter.messagebox as tkm
+
+def main():
+    root = tk.Tk()
+    root.title("迷えるこうかとん")
+    root.geometry("1500x900")
+
+    root.mainloop()    
+
+if __name__ == "__main__":
+    main()
