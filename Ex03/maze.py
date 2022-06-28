b8d8ba68c38185b9a321ad0691d1d268b4600783
RinShigekane <c0a2106423@edu.teu.ac.jp>
2022-06-28 16:37:21 +0900

    関数追加 ゴール判定goal() スタートマスをlimeに塗るstart_p()

--- Ex03/maze.py
+++ Ex03/maze.py
@@ -1,11 +1,10 @@
-from itertools import count
 import random
 import tkinter as tk
 import tkinter.messagebox as tkm
 import maze_maker
 
 def main():
-    global root, canvas, koukaton, mx, my, cx, cy, key, map
+    global root, canvas, koukaton, mx, my, cx, cy, key, map, goal_pos
     width = 1500
     height = 900
     key = ""
@@ -22,10 +21,14 @@ def main():
     map = maze_maker.make_maze(width//100, height//100)
     maze_maker.show_maze(canvas, map)
 
-    goal_p = goal_p()
+    goal_pos = goal_p()
+    #print(goal_pos)
 
     koukaton = tk.PhotoImage(file = "fig/5.png")
     mx, my = 1,1
+
+    start_p(mx, my)
+
     cx, cy = mx*100+50, my*100+50
     canvas.create_image(cx, cy, image=koukaton, tag="koukaton")
 
@@ -46,6 +49,10 @@ def key_up(event):
     #print(key)
     return
 
+def start_p(x, y):
+    canvas.create_rectangle(y*100, x*100, y*100+100, x*100+100, 
+                            fill="lime")
+
 def goal_p():
     global map, canvas
     height = len(map)
@@ -57,29 +64,31 @@ def goal_p():
             if map[i][-3] + map[i-1][-2] + map[i+1][-2] == 2:
                 end_p.append(i)
     #print(end_p)
-    if end_p:
-        i = random.randint(0,len(end_p))
+    if len(end_p):
+        i = random.randint(0,len(end_p)-1)
+        #print(i)
         x = end_p[i]
     else:
-        for i in range(len(map)):
+        for i in range(height):
             if not map[i][-2]:
                 x = i
         else:
-            for i in range(len(map)):
+            for i in range(height):
                 if not map[i][-3]:
                     x = i
     y = len(map[-2])-2
     #print(x, y)
     canvas.create_rectangle(y*100, x*100, y*100+100, x*100+100, 
                             fill="red")
-    return(x, y)
+    return(y, x)
 
 def goal():
     global root
-    root.destroy
+    tkm.showinfo("通知", "ゴールです！おめでとう！\nOKボタンを押してプログラムを終了")
+    root.destroy()
 
 def main_proc():
-    global canvas, mx, my, cx, cy, key, map
+    global canvas, mx, my, cx, cy, key, map, goal_pos
     if key == "Up":
         my -= 1
         tag = [0, 1]
@@ -99,6 +108,9 @@ def main_proc():
     else:
         mx += tag[0]
         my += tag[1]
+    #print(mx ,my)
+    if (mx, my) == goal_pos:
+        goal()
     root.after(100, main_proc)
 
 if __name__ == "__main__":
