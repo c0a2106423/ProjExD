3b1f194a41df7876630ad24de6bc3b7402e5c073
RinShigekane <c0a2106423@edu.teu.ac.jp>
2022-07-05 14:20:26 +0900

    Kadai01

--- Ex04/dodge_bomb.py
+++ Ex04/dodge_bomb.py
@@ -1,3 +1,4 @@
+from operator import truediv
 from turtle import width
 import pygame as pg
 import sys
@@ -15,16 +16,30 @@ class game():
         width = 900
         global clock
         clock = pg.time.Clock()
-        bg_img  = pg.image.load("fig/pg_bg.jpg")
+
         pg.display.set_caption("逃げろ！こうかとん")
-        screen = pg.display.set_mode((height, width))
-        bg = obj(screen, bg_img, height//2, width//2)
+        screen = self.setScreen(height, width)
+        
+
+        while True:
+            for event in pg.event.get():
+                if event.type == pg.QUIT: return
+                elif event.type == pg.KEYDOWN:
+                    pass
+            pg.display.update()
 
         tori_img = pg.image.load("fig/6.png")
         tori_img = pg.transform.rotozoom(tori_img, 0, 2.0)
+        #tori01 = obj(screen, tori_img, 600, 400)
+    
+    def setScreen(self, height, width):
+        screen = pg.display.set_mode((height, width))
+        screen_rect = screen.get_rect()
+        bg_img  = pg.image.load("fig/pg_bg.jpg")
+        bg_rect = bg_img.get_rect()
+        screen.blit(bg_img, bg_rect)
+        return screen
 
-        tori01 = obj(screen, tori_img, 600, 400)
-        clock.tick(0.2)
 
 class obj():
     def __init__(self, screen, img, x=0, y=0) -> None:
