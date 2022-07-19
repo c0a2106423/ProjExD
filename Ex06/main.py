7fd0cf7e31169b616c0a29c6b5b59250d6f29c4c
RinShigekane <c0a2106423@edu.teu.ac.jp>
2022-07-19 16:53:47 +0900

    コメント追加

--- Ex06/main.py
+++ Ex06/main.py
@@ -9,53 +9,91 @@ class Screen:
         pg.display.set_caption(title)
         self.sfc = pg.display.set_mode(wh) # Surface
         self.rct = self.sfc.get_rect()            # Rect
-        self.x_count = wh[0]//100
-        self.y_count = wh[1]//100
+        self.x_count = wh[0]//100 #よこのブロック数
+        self.y_count = wh[1]//100 #たてのブロック数
 
-        self.map_init(self.sfc, self.x_count, self.y_count, wh)
+        self.map_init(self.sfc, self.x_count, self.y_count, wh)#迷路生成
 
     def blit(self):
-        self.sfc.fill((255,255,255))
+        self.sfc.fill((255,255,255))#背景を白色に染める
+        self.s_tile_blit()#特殊マスの描写
         pass
-        self.map_blit()
+        self.map_blit()#黒ブロック描写(絶対にキャラがかぶらないならいらない)
 
     def map_init(self, base_obj, x:int, y:int, wh:tuple) -> None:
         self.map_ary = maze_maker.make_maze(x, y)
         self.x_size = round(wh[0]/x)
         self.y_size = round(wh[1]/y)
-        #self.w_color = (255, 255, 255)
-        self.b_color = (  0,   0,   0)
-        self.block_lst = list()
         for i in self.map_ary:
             print(i)
-        
-        for i in range(len(self.map_ary)):
-            for j in range(len(self.map_ary[i])):
-                if self.map_ary[i][j]==1:
-                    self.block_lst.append(Block(self.b_color, self.x_size, self.y_size, (j, i), self.sfc))
-        pg.display.update()
 
-    def map_blit(self):
-        for i in self.block_lst:
+        self.map_ary[1][1] = 2
+        goal_pos = self.goal_p()
+        self.map_ary[goal_pos[1]][goal_pos[0]] = 3
+        self.b_color = (100, 100, 100)
+        self.s_color = (  0, 255,   0)
+        self.g_color = (255,   0,   0)
+        self.block_lst = list() #壁tileのリスト
+        self.s_tile_lst = list() #superなtileのリスト
+        
+        for i in range(len(self.map_ary)):#マップ配列を回す。
+            for j in range(len(self.map_ary[i])):
+                if self.map_ary[i][j]==1: # 壁のマスならば、Blockクラスのインスタンスを生成し、block_lstリストに追加。
+                    self.block_lst.append(Block(self.b_color, self.x_size, self.y_size, (j, i), self.sfc)) 
+                #特殊な役割のtileならば、Blockクラスのインスタンスを生成し、s_tile_lstリストに追加。
+                elif self.map_ary[i][j] == 2: #スタートなら
+                    self.s_tile_lst.append(Block(self.s_color, self.x_size, self.y_size, (j, i), self.sfc)) 
+                elif self.map_ary[i][j] == 3: #ゴールなら
+                    self.s_tile_lst.append(Block(self.g_color, self.x_size, self.y_size, (j, i), self.sfc)) 
+                    print("fiz")
+                    
+        pg.display.update()#初期描写
+
+    def map_blit(self):#（壁を描写）
+        for i in self.block_lst: #(1つずつ)丁寧に描く。効率よいやり方求む。
             i.blit(self.sfc)
+    
+    def s_tile_blit(self):#特殊なタイルを描写.
+        for i in self.s_tile_lst:
+            i.blit(self.sfc)
+    
+    def goal_p(self):
+        height = len(self.map_ary) #高さを取得
+        end_p=[] #ゴール候補のy座標のリスト
+        for i in range(0, height):
+            if not self.map_ary[i][-2]: #そのマスが床なら
+                if self.map_ary[i][-3] + self.map_ary[i-1][-2] + self.map_ary[i+1][-2] == 2: #かつ、右側を除く3方のうち、2方が壁なら
+                    end_p.append(i) # 候補リストに追加
+        #print(end_p)
+        if len(end_p): #候補が一つでもあれば
+            i = random.randint(0,len(end_p)-1) #リストのインデックスをランダムに選び
+            #print(i)
+            y = end_p[i] # y に代入
+        else: #候補がない場合
+            for i in range(height): #とりあえず右端から２列目の床のマスにゴールを設定
+                if not self.map_ary[i][-2]:
+                    y = i
+        x = len(self.map_ary[-2])-2 #x座標を設定
+        print(x, y)
+        return(x, y) #設定した座標を返す
 
 
 class Block:
-    def __init__(self, color, x_size, y_size, xy, base_obj:Screen):
-        self.x_size = x_size
-        self.y_size = y_size
+    def __init__(self, color, x_size, y_size, xy, base_obj:Screen):#
+        self.x_size = x_size #念のため持たせてる。いらない
+        self.y_size = y_size #同上
         #print(xy)
 
-        self.sfc = pg.Surface((self.x_size, self.y_size)) 
-        self.rct = self.sfc.get_rect()
+        self.sfc = pg.Surface((x_size, y_size)) #surfaceを1マス分の大きさに設定
+        self.rct = self.sfc.get_rect() # surfaceの大きさでrectを作っておく
 
-        pg.draw.rect(self.sfc, color, (0,0,self.x_size, self.y_size))
-        self.rct = self.sfc.get_rect() 
-        self.rct.centerx = xy[0] * x_size + x_size/2
-        self.rct.centery = xy[1] * y_size + y_size/2
-
+        pg.draw.rect(self.sfc, color, (0,0,x_size, y_size)) #
+        self.rct = self.sfc.get_rect() #
+        self.rct.centerx = xy[0] * x_size + x_size/2 #
+        self.rct.centery = xy[1] * y_size + y_size/2 #
+ 
     def blit(self, base_obj):
-        base_obj.blit(self.sfc, self.rct)
+        base_obj.blit(self.sfc, self.rct) #ブロックを背景に貼り付ける。
         pass
 
 
@@ -69,30 +107,43 @@ class Bird:
     def blit(self, base_obj: Screen):
         base_obj.sfc.blit(self.sfc, self.rct)
     
-    def update(self, base_obj: Screen, map_ary):
+    def update(self, base_obj: Screen):
+        self.dx = base_obj.x_size
+        self.dy = base_obj.y_size
         key_states = pg.key.get_pressed() # 辞書
         if key_states[pg.K_UP] : 
-            self.rct.centery -= base_obj.y_size
+            self.rct.centery -= self.dy
         if key_states[pg.K_DOWN] : 
-            self.rct.centery += base_obj.y_size
+            self.rct.centery += self.dy
         if key_states[pg.K_LEFT] : 
-            self.rct.centerx -= base_obj.x_size
+            self.rct.centerx -= self.dx
         if key_states[pg.K_RIGHT] : 
-            self.rct.centerx += base_obj.x_size
+            self.rct.centerx += self.dx
+        
         if collision_detect(self.rct, base_obj.block_lst) or check_bound(self.rct, base_obj.rct) != (1, 1): # 領域外だったら
-            if key_states[pg.K_UP]    == True: self.rct.centery += base_obj.y_size
-            if key_states[pg.K_DOWN]  == True: self.rct.centery -= base_obj.y_size
-            if key_states[pg.K_LEFT]  == True: self.rct.centerx += base_obj.x_size
-            if key_states[pg.K_RIGHT] == True: self.rct.centerx -= base_obj.x_size
+            if key_states[pg.K_UP]    == True: self.rct.centery += self.dy
+            if key_states[pg.K_DOWN]  == True: self.rct.centery -= self.dy
+            if key_states[pg.K_LEFT]  == True: self.rct.centerx += self.dx
+            if key_states[pg.K_RIGHT] == True: self.rct.centerx -= self.dx
         self.blit(base_obj)
         
 class Enemy(Bird):
     def __init__(self, image: str, zoom_rate: float, xy: tuple) -> None:
         super().__init__(image, zoom_rate, xy)
+        self.now_dir = 0
     
-    def update(self, base_obj: Screen, map_obj):
-        #if
+    def update(self, base_obj: Screen, map_ary):
         pass
+
+    def search_left(self, map_ary):
+        dif = ((0,1), (1,0), (0,-1),(-1,0))
+        for i in range(4):
+            dir = (self.now_dir+3+i)%4
+            x = self.rct.centerx + dif[dir][0]*self.dx
+            y = self.rct.centery + dir[dir][1]*self.dy
+            if map_ary[x][y] == 0 or map_ary[x][y] == 3:
+                break
+        return dir,x,y
 
 
 class main: # mainをクラスに。
@@ -106,12 +157,16 @@ class main: # mainをクラスに。
         while True:
             bird.blit(scr)
             for event in pg.event.get():
-                if event.type == pg.QUIT: 
-                    return
-                if event.type == pg.KEYDOWN:
+                if event.type == pg.QUIT: #右上のXが押されたら
+                    return                  #mainから抜けてプログラムを終了させる
+                if event.type == pg.KEYDOWN: #キーが押下されたならば、
+                    #方向キーならば、
                     if event.key == pg.K_DOWN or event.key == pg.K_UP or event.key == pg.K_LEFT or event.key==pg.K_RIGHT:
-                        bird.update(scr, scr.map_ary)
+                        bird.update(scr, scr.map_ary) #鳥を移動させ、描写しなおす
             enemy.update(scr, scr.map_ary)
+            if bird.rct.colliderect(scr.s_tile_lst[1].rct):
+                main()
+                return
             pg.display.update()
             clock.tick(1000)
             scr.blit()
@@ -128,15 +183,24 @@ class main: # mainをクラスに。
                 if event.type == pg.QUIT:
                     return
 
-def collision_detect(rct, block_rct:list):
-    bool = False
-    for b in block_rct:
-        if rct.colliderect(b.rct) == True:
-            print("True")
-            bool = True
-            break
-        else :
-            print("False")
+def collision_detect(rct, block_rct):
+    bool = False #初期値を設定
+    if type(block_rct)==list:#もしリスト型で渡されたら、要素ごとに判定。
+        for b in block_rct:
+            if rct.colliderect(b.rct) == True:
+                #print("True")
+                bool = True
+                break
+            else :
+                #print("False")
+                continue
+    else: #違うなら、引数をそのまま判定
+        try: #一応try文つき。
+            if rct.colliderect(block_rct) == True:
+                #print("True")
+                bool = True
+        except:
+            print("ここに来たらおかしい。")
     return bool
 # 練習7
 def check_bound(rct, scr_rct):
